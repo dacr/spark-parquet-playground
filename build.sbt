069212507572398e714666eaccb8d1cd526b11f5
crosson.david@gmail.com <crosson.david@gmail.com>
2016-09-01 23:18:01 +0200

    tbc

---
+++ build.sbt
@@ -0,0 +1,37 @@
+name := "spark-parquet-playground"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation" , "-feature", "-language:implicitConversions")
+
+mainClass in assembly := Some("dummy.Dummy")
+
+jarName in assembly := "dummy.jar"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.0"
+)
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.12" % "test"
+
+initialCommands in console := """import dummy._"""
+
+sourceGenerators in Compile <+= 
+ (sourceManaged in Compile, version, name, jarName in assembly) map {
+  (dir, version, projectname, jarexe) =>
+  val file = dir / "dummy" / "MetaInfo.scala"
+  IO.write(file,
+  """package dummy
+    |object MetaInfo { 
+    |  val version="%s"
+    |  val project="%s"
+    |  val jarbasename="%s"
+    |}
+    |""".stripMargin.format(version, projectname, jarexe.split("[.]").head) )
+  Seq(file)
+}
+
---
+++ src/main/scala/dummy/Dummy.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 David Crosson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dummy
+
+object Dummy {
+  val userName = util.Properties.userName
+  val message = s"Hello ${userName}."
+  def main(args:Array[String]):Unit = {
+    
+    import org.apache._    
+    
+    // Create a simple DataFrame, store into a partition directory
+    val squaresDF = spark.sparkContext.makeRDD(1 to 5).map(i => (i, i * i)).toDF("value", "square")
+    squaresDF.write.parquet("data/test_table/key=1")
+
+    // Create another DataFrame in a new partition directory,
+    // adding a new column and dropping an existing column
+    val cubesDF = spark.sparkContext.makeRDD(6 to 10).map(i => (i, i * i * i)).toDF("value", "cube")
+    cubesDF.write.parquet("data/test_table/key=2")
+
+    // Read the partitioned table
+    val mergedDF = spark.read.option("mergeSchema", "true").parquet("data/test_table")
+    mergedDF.printSchema()
+    
+    
+  }
+}
